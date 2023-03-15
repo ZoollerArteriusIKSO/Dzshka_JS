708f808d4700c06092060eee7f2f1704317f216c
Артьом Зайденцаль <82270682+ZoollerArteriusIKSO@users.noreply.github.com>
2023-03-15 19:06:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function create (tag, properties, styles) {
+
+    const body = document.querySelector('body');
+    let g = document.createElement(tag);
+    body.append(g);
+        
+    for (let propertie of properties ) {
+        
+        if ( propertie[0] === 'innerHTML' ) {
+            g.innerHTML = propertie[1]; 
+        }
+        if (propertie[0] === 'type') {
+            g.type = propertie[1]; 
+        }
+        if (propertie[0] === 'placeholder') {
+            g.placeholder = propertie[1];
+        }
+    }
+    
+    for (let style of styles) {
+        if (style[0] === 'fontSize'){
+            g.style.fontSize = style[1];
+        }
+        if (style[0] === 'color'){
+            g.style.color = style[1];
+        }
+    }    
+}
+create("input",[ ['innerHTML', 'Lorem ipsum'], ['type', 'text'], ['placeholder', 'dolor sit amet'] ],[ ['fontSize', '18px'], ['color', 'red'] ]);
